b624363e545b9b1ab7a8c211df84de22d8529938
Fabien MARTY <fabien.marty@meteo.fr>
2015-04-24 15:07:26 +0200

    add some doc

--- thr/http2redis/__init__.py
+++ thr/http2redis/__init__.py
@@ -10,9 +10,10 @@ class HTTPExchange(object):
     Encapsulate an HTTP request/response exchange.
 
     Attributes:
-        request: A Tornado HTTPServerRequest object
+        request: A Tornado HTTPServerRequest object.
         response: a dict holding attributes that will be set on the response
-        queue: the name of the Redis queue where to push the request
+            (headers, status_code and body are valid keys).
+        queue: the name of the Redis queue where to push the request.
     """
 
     def __init__(self, request):
