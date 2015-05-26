04241cb129d1b52ed30372f2c9f9227286311d5f
Fabien MARTY <fabien.marty@meteo.fr>
2015-05-26 08:54:41 +0200

    new methods to prepare refactoring

--- thr/redis2http/counter.py
+++ thr/redis2http/counter.py
@@ -8,10 +8,10 @@ from collections import defaultdict
 
 
 counters = defaultdict(int)
+counters_blocks = defaultdict(int)
 
 
 def get_counter(counter):
-    global counters
     return counters[counter]
 
 
@@ -24,6 +24,25 @@ def incr_counters(counter_list):
     global counters
     for counter in counter_list:
         counters[counter] += 1
+
+
+def get_counter_blocks(counter):
+    return counters_blocks[counter]
+
+
+def conditional_incr_counters(conditions):
+    global counters, counters_blocks
+    blocked_counters = []
+    for counter_name, limit in conditions:
+        if limit <= counters[counter_name]:
+            blocked_counters.append(counter_name)
+            counters_blocks[counter_name] += 1
+    if len(blocked_counters) == 0:
+        tmp = [x[0] for x in conditions]
+        incr_counters(tmp)
+        return (True, tmp)
+    else:
+        return (False, blocked_counters)
 
 
 def decr_counters(counter_list):
